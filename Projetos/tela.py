f72a7407eae8075ba929a09bca2264b9100c5011
JOÃO LUIZ RIBEIRO GARBINI <43076430+Joaogarbini@users.noreply.github.com>
2021-02-19 18:51:20 -0300

    Update tela.py

--- Projetos/tela.py
+++ Projetos/tela.py
@@ -4,9 +4,11 @@ class TelaPython:
         #Layout
         layout = [
             [sg.Text("Nome",size=(5,0)),sg.Input(size=(15,0),key='nome')],
-            [sg.Text("Idade",size=(5,0)),sg.Input(size=(15,0)key='idade')],
+            [sg.Text("Idade",size=(5,0)),sg.Input(size=(15,0),key='idade')],
             [sg.Text('Quais provedores de e-mail são aceitos?')],
             [sg.Checkbox('Gmail',key='gmail'),sg.Checkbox('Outlook',key='outlook'),sg.Checkbox('Yahoo',key='yahoo')],
+            [sg.Text("Aceita Cartão")],
+            [sg.Radio("Sim",'cartoes', key='aceitaCartao'),sg.Radio('Não','cartoes',key='naoAceitaCartao')],
             [sg.Button("Enviar Dados")]
         ]
         #janela
@@ -21,11 +23,15 @@ class TelaPython:
         aceita_gmail =self.values['gmail']
         aceita_outlook =self.values['outlook']
         aceita_yahoo =self.values['yahoo']
+        aceita_cartao =self.values['aceitaCartao']
+        nao_aceita_cartao =self.values['naoAceitaCartao']
         print(f'nome: {nome}')
         print(f'idade: {idade}')
         print(f'aceita gmail: {aceita_gmail}')
-        print(f'aceita outlook: {aceita_outlook}}')
+        print(f'aceita outlook: {aceita_outlook}')
         print(f'aceita yahoo: {aceita_yahoo}')
+        print(f'Aceita Cartão: {aceita_cartao}')
+        print(f'Não Aceita Cartão: {nao_aceita_cartao}')
      
 
 tela = TelaPython()
