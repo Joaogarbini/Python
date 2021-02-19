eaa5783515e9ecec945e92229ed767a61d0f6988
JOÃO LUIZ RIBEIRO GARBINI <43076430+Joaogarbini@users.noreply.github.com>
2021-02-19 18:31:43 -0300

    Update tela.py

--- Projetos/tela.py
+++ Projetos/tela.py
@@ -3,8 +3,10 @@ class TelaPython:
     def __init__(self):
         #Layout
         layout = [
-            [sg.Text("Nome",size=(5,0)),sg.Input(size=(15,0))],
-            [sg.Text("Idade",size=(5,0)),sg.Input(size=(15,0))],
+            [sg.Text("Nome",size=(5,0)),sg.Input(size=(15,0),key='nome')],
+            [sg.Text("Idade",size=(5,0)),sg.Input(size=(15,0)key='idade')],
+            [sg.Text('Quais provedores de e-mail são aceitos?')],
+            [sg.Checkbox('Gmail',key='gmail'),sg.Checkbox('Outlook',key='outlook'),sg.Checkbox('Yahoo',key='yahoo')],
             [sg.Button("Enviar Dados")]
         ]
         #janela
@@ -14,7 +16,17 @@ class TelaPython:
         self.button, self.values = janela.Read()
 
     def Iniciar(self):
-        print(self.values)
+        nome =self.values['nome']
+        idade =self.values['idade']
+        aceita_gmail =self.values['gmail']
+        aceita_outlook =self.values['outlook']
+        aceita_yahoo =self.values['yahoo']
+        print(f'nome: {nome}')
+        print(f'idade: {idade}')
+        print(f'aceita gmail: {aceita_gmail}')
+        print(f'aceita outlook: {aceita_outlook}}')
+        print(f'aceita yahoo: {aceita_yahoo}')
+     
 
 tela = TelaPython()
 tela.Iniciar()        
